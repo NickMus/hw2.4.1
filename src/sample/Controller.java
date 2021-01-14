8b1ce936c1bb2f944002086ed3bb67950d6f6aed
NickMus <n.musinov@gmail.com>
2021-01-14 13:57:59 +0300

    hw 2.4

---
+++ src/sample/Controller.java
@@ -0,0 +1,50 @@
+package sample;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+    public class Controller {
+
+        @FXML
+        private ResourceBundle resources;
+
+        @FXML
+        private URL location;
+
+        @FXML
+        private TextArea textArea;
+
+        @FXML
+        private AnchorPane usersField;
+
+        @FXML
+        private Button sendButton;
+
+        @FXML
+        private TextField sendTextField;
+
+        @FXML
+        void initialize() {
+
+        }
+
+        public void sendMessage() {
+            textArea.appendText("Я: " + sendTextField.getText() + "\n");
+            sendTextField.clear();
+            sendTextField.requestFocus();
+        }
+
+        public void sendByEnter() {
+            sendMessage();
+        }
+    }
+
+
